dbd650393e4a71c91a5a67024166cce54d1412cd
PROTTASAKARIM <prottasakarim99@gmail.com>
2022-12-11 15:53:21 +0600

    working on damage

--- models/damageModel.js
+++ models/damageModel.js
@@ -2,15 +2,27 @@ const mongoose = require("mongoose");
 
 const damageSchema = mongoose.Schema(
   {
-    product: { type: mongoose.Types.ObjectId, ref: "Product", require: true },
+    products: [
+      {
+        type: Map,
+        of: new mongoose.Schema({
+          priceId: { type: mongoose.Types.ObjectId, ref: "Price" },
+          qty: { type: Number, require: true },
+          article_code: { type: String, require: true },
+          name: { type: String, require: true },
+          tp: { type: String, require: true },
+          reason: { type: String },
+        }),
+      },
+    ],
     warehouse: {
       type: mongoose.Types.ObjectId,
       ref: "Warehouse",
       require: true,
     },
-    qty: { type: Number, require: true },
-    reason: { type: String },
-    priceId: { type: mongoose.Types.ObjectId, ref: "Price" },
+    note: { type: String },
+    total: { type: Number, require: true },
+    totalItem: { type: Number, require: true },
     userId: { type: mongoose.Types.ObjectId, ref: "User", require: true },
     status: {
       type: String,
