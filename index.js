b2239d7f63c79802da1eedddb324fda9ca2a3a06
Atiqule Islam <manishankarvakta@egmail.com>
2022-12-26 15:30:24 +0600

    update database url

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(
 app.use(express.json());
 app.use(express.static(__dirname + "/template"));
 
-const dbUrl = `mongodb+srv://posdb-user:IOK6faeoehDttV7os@cluster0.jmnaf.mongodb.net/pos-api-v1?retryWrites=true&w=majority`;
+const dbUrl = `mongodb+srv://posdb-user:IOK6faeoehDttV7o@cluster0.jmnaf.mongodb.net/pos-api-v1?retryWrites=true&w=majority`;
 // const dbUrl = `mongodb+srv://pos-tester:vgQJZDIxungr8ywt@cluster0.3sihhom.mongodb.net/?retryWrites=true&w=majority`;
 // const dbUrl = `mongodb+srv://pos-tester:vgQJZDIxungr8ywt@cluster0.3sihhom.mongodb.net/?retryWrites=true&w=majority`;
 // const dbUrl = `mongodb+srv://tcm:rXWmC6cp4Q3FZ2ef@cluster0.0s2yh4f.mongodb.net/?retryWrites=true&w=majority`;
