f707db9fd637c6762ea1b7da17032146992e0252
PROTTASAKARIM <prottasakarim99@gmail.com>
2023-01-07 14:34:26 +0600

    handle price done

--- middlewares/handlePrice.js
+++ middlewares/handlePrice.js
@@ -1,67 +1,75 @@
+// const { default: id } = require("date-fns/esm/locale/id/index.js");
 const Price = require("../models/priceModel");
 const handleNewPrice = async (req, res, next) => {
     // console.log("new Price", req.body)
     let grnData = req.body;
     let products = grnData.products;
-    let updatedProducts = []
+    // let updatedProducts = []
     let upProducts = []
     let temp = false;
     // console.log("grn products", grnData.products)
 
-    products?.map(async product => {
-        // console.log("single products", product)
-        // let price = {};
+    // LOOP
+    const updatedProducts = products?.map(async product => {
+        // CHECK PRODUCT PRICE
         if (product.newPrice === true) {
             const price = {
                 article_code: product.id,
                 supplier: grnData.supplier,
                 warehouse: grnData.warehouse,
-                // newPrice: false,
+                newPrice: false,
                 tp: product.tp,
                 mrp: product.mrp,
                 status: "active",
             };
-            // console.log(price)
-            // console.log(product.id)
+
+            // NEW PRODUCT PRICE
             const newPrice = new Price(price)
-            let savePrice = await newPrice.save();
-            if (savePrice) {
-                // console.log("savePrice", savePrice)
-                const rest = products.filter((pro) => pro.article_code != product.article_code)
+            return new Promise(async (resolve, reject) => {
+                await newPrice.save((error) => {
+                    if (error) {
+                        console.log(error);
+                        reject(error);
+                    } else {
+                        //    IF SAVE NEW PRICE
 
-                // console.log("rest", rest)
+                        // console.log("new price", newPrice)
+                        // console.log("products", product)
+                        // console.log("rest", rest)
+                        resolve({ ...product, priceId: newPrice._id, newPrice: false })
+                    }
+                });
+            });
 
-                upProducts = [...rest,
-                {
-                    ...product,
-                    priceId: savePrice._id,
-                    newPrice: false,
-                }]
-                // console.log("newProductList", products)
-                updatedProducts = [...products];
-                if (updatedProducts.length > 0) {
-                    temp = true
-                }
-
-                console.log("updated", updatedProducts)
-                // req.body.products = products;
-            }
 
         } // Data match and create new price
 
+
     }) //Loop END
+    Promise.all(updatedProducts)
+        .then(result => {
+            console.log("result", result)
+            // let productData = req.body.products;
 
+            let newProductList;
+            result.map(pro => {
+                // console.log(pro)
+                if (pro !== undefined) {
+                    // GET ALL PRODUCTS WITHOUT THIS PRODUCT
+                    console.log("pro", pro)
+                    console.log("products", products)
+                    const rest = products.filter((pro1) => pro1.id != pro.id)
+                    console.log("rest", rest)
+                    products = [...rest, pro]
 
+                }
+            })
 
-
-    if (temp) {
-        req.body.newProducts = updatedProducts;
-        next()
-    }
-
-    // console.log("grn dATA", req.body)
-
-
+            console.log("newProductList", products)
+            req.body.newProducts = products
+            console.log(req.body)
+        })
+        .catch(err => { console.log(err) })
 }
 module.exports = {
     handleNewPrice
