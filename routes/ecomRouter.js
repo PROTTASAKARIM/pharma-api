f6564aafd0bb53edc27a02be9065c52497810585
PROTTASAKARIM <prottasakarim99@gmail.com>
2023-01-24 19:28:54 +0600

    working on ecom backed

---
+++ routes/ecomRouter.js
@@ -0,0 +1,385 @@
+const express = require("express");
+const expressAsyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const Sale = require("../models/saleModel");
+const Product = require("../models/productModel");
+const Category = require("../models/categoryModel");
+const checklogin = require("../middlewares/checkLogin");
+const { generatePosId } = require("../middlewares/generateId");
+const { startOfDay, endOfDay } = require("date-fns");
+const ecomRouter = express.Router();
+
+
+
+/*==========================
+* PRODUCT
+===========================*/
+ecomRouter.get(
+    "/product/category/:id/:page/:size",
+    expressAsyncHandler(async (req, res) => {
+        const category = req.params.id;
+        const page = parseInt(req.params.page);
+        const size = parseInt(req.params.size);
+        const queryString = req.query?.q?.trim().toString().toLocaleLowerCase();
+        const currentPage = parseInt(page) + 0;
+
+        let query = {};
+        let product = [];
+        // const size = parseInt(req.query.size);
+        console.log("page:", currentPage, "size:", size, "search:", queryString);
+        console.log(typeof queryString);
+
+        //check if search or the pagenation
+
+        if (queryString) {
+            console.log("== query");
+
+            console.log("search:", query);
+            // search check if num or string
+            const isNumber = /^\d/.test(queryString);
+            console.log(isNumber);
+            if (!isNumber) {
+                // if text then search name
+                query = {
+                    category: category,
+                    name: { $regex: new RegExp(".*" + queryString + ".*?", "i") },
+                };
+                // query = { name:  queryString  };
+            } else {
+                // if number search in ean and article code
+                query = {
+                    $or: [
+                        {
+                            category: category,
+                            ean: { $regex: RegExp("^" + queryString + ".*", "i") }
+                        },
+                        {
+                            category: category,
+                            article_code: {
+                                $regex: RegExp("^" + queryString + ".*", "i"),
+                            },
+                        },
+                    ],
+                };
+            }
+            console.log(query);
+
+            product = await Product.find(query)
+                .select({
+                    _id: 1,
+                    name: 1,
+                    ean: 1,
+                    unit: 1,
+                    article_code: 1,
+                    priceList: 1,
+                    category: 1,
+                    promo_price: 1,
+                    promo_start: 1,
+                    promo_end: 1,
+                    promo_type: 1
+                })
+                .limit(100)
+                .populate("category", "name")
+                .populate("priceList");
+            res.status(200).json(product);
+        } else {
+            console.log("no query");
+
+            // regular pagination
+            query = { category: category };
+
+            product = await Product.find(query)
+                .select({
+                    _id: 1,
+                    name: 1,
+                    ean: 1,
+                    unit: 1,
+                    article_code: 1,
+                    priceList: 1,
+                    category: 1,
+                    promo_price: 1,
+                    promo_start: 1,
+                    promo_end: 1,
+                    promo_type: 1
+                })
+                .limit(size)
+                .skip(size * page)
+                .populate("category", "name")
+                .populate("priceList");
+            res.status(200).json(product);
+            console.log("done:", query);
+        }
+    })
+);
+// ecomRouter.get(
+//     "/product/category/:id",
+//     expressAsyncHandler(async (req, res) => {
+//         const category = req.params.id;
+//         const products = await Product.find({ category: category })
+//             .select({
+//                 _id: 1,
+//                 name: 1,
+//                 ean: 1,
+//                 unit: 1,
+//                 vat: 1,
+//                 article_code: 1,
+//                 priceList: 1,
+//                 promo_price: 1,
+//                 promo_start: 1,
+//                 promo_end: 1,
+//                 promo_type: 1
+//             })
+//             .populate("priceList", "mrp");
+//         // .limit(5);
+//         // Select fields
+//         // console.log(products);
+//         // console.log(category);
+//         res.send(products);
+//     })
+// );
+
+ecomRouter.get(
+    "/product/:page/:size",
+    expressAsyncHandler(async (req, res) => {
+        const page = parseInt(req.params.page);
+        const size = parseInt(req.params.size);
+        const queryString = req.query?.q?.trim().toString().toLocaleLowerCase();
+        const currentPage = parseInt(page) + 0;
+
+        let query = {};
+        let product = [];
+        // const size = parseInt(req.query.size);
+        console.log("page:", currentPage, "size:", size, "search:", queryString);
+        console.log(typeof queryString);
+
+        //check if search or the pagenation
+
+        if (queryString) {
+            console.log("== query");
+
+            console.log("search:", query);
+            // search check if num or string
+            const isNumber = /^\d/.test(queryString);
+            console.log(isNumber);
+            if (!isNumber) {
+                // if text then search name
+                query = {
+                    name: { $regex: new RegExp(".*" + queryString + ".*?", "i") },
+                };
+                // query = { name:  queryString  };
+            } else {
+                // if number search in ean and article code
+                query = {
+                    $or: [
+                        { ean: { $regex: RegExp("^" + queryString + ".*", "i") } },
+                        {
+                            article_code: {
+                                $regex: RegExp("^" + queryString + ".*", "i"),
+                            },
+                        },
+                    ],
+                };
+            }
+            console.log(query);
+
+            product = await Product.find(query)
+                .select({
+                    _id: 1,
+                    name: 1,
+                    ean: 1,
+                    unit: 1,
+                    article_code: 1,
+                    priceList: 1,
+                    category: 1,
+                    promo_price: 1,
+                    promo_start: 1,
+                    promo_end: 1,
+                    promo_type: 1
+                })
+                .limit(100)
+                .populate("category", "name")
+                .populate("priceList");
+            res.status(200).json(product);
+        } else {
+            console.log("no query");
+
+            // regular pagination
+            query = {};
+
+            product = await Product.find(query)
+                .select({
+                    _id: 1,
+                    name: 1,
+                    ean: 1,
+                    unit: 1,
+                    article_code: 1,
+                    priceList: 1,
+                    category: 1,
+                    promo_price: 1,
+                    promo_start: 1,
+                    promo_end: 1,
+                    promo_type: 1
+                })
+                .limit(size)
+                .skip(size * page)
+                .populate("category", "name")
+                .populate("priceList");
+            res.status(200).json(product);
+            console.log("done:", query);
+        }
+    })
+);
+
+ecomRouter.get(
+    "/ecom/product/details/:id",
+    expressAsyncHandler(async (req, res) => {
+        const id = req.params.id;
+        const products = await Product.find({ _id: id })
+            .select({
+                _id: 1,
+                name: 1,
+                ean: 1,
+                vat: 1,
+                unit: 1,
+                article_code: 1,
+                priceList: 1,
+                ean: 1,
+                promo_start: 1,
+                promo_end: 1,
+                promo_price: 1,
+                promo_type: 1
+            })
+            .populate("priceList", { mrp: 1, tp: 1, supplier: 1, _id: 1, status: 1 });
+        res.send(products[0]);
+    })
+);
+/*==========================
+* CATEGORY
+===========================*/
+// GET ALL CATEGORY BY GROUP
+ecomRouter.get(
+    "/category/group/:group",
+    expressAsyncHandler(async (req, res) => {
+        const group = req.params.group.toLowerCase();
+        const categories = await Category.find({ group: group });
+        res.send(categories);
+    })
+);
+
+// GET ALL CATEGORY BY MASTER CATEGORY
+ecomRouter.get(
+    "/category/master/:mcId",
+    expressAsyncHandler(async (req, res) => {
+        const mcId = req.params.mcId;
+        const categories = await Category.find({
+            mcId: mcId,
+            mc: { $exists: true },
+        });
+        res.send(categories);
+    })
+);
+
+
+
+/*==========================
+* SALE
+===========================*/
+/*==========================
+* CUSTOMERS
+===========================*/
+// GET ALL sales
+ecomRouter.get(
+    "/sale",
+    expressAsyncHandler(async (req, res) => {
+        const sales = await Sale.find({
+            status: "complete",
+            source: 'web'
+        })
+            .select({
+                invoiceId: 1,
+                totalItem: 1,
+                grossTotalRound: 1,
+                total: 1,
+                status: 1,
+                billerId: 1,
+                createdAt: 1,
+                changeAmount: 1,
+            })
+            .populate("billerId", "name");
+        res.send(sales);
+        // // res.send('removed');
+    })
+);
+ecomRouter.post(
+    "/sale",
+    generatePosId,
+    // updateInventoryOutOnSaleIn,
+    expressAsyncHandler(async (req, res) => {
+        console.log("body", req.body);
+        let newSale = new Sale(req.body);
+        console.log("newSale", newSale);
+        try {
+            await newSale.save((err, sale) => {
+                if (err) {
+                    console.log("err:", res, err);
+                    res
+                        .status(500)
+                        .json({ message: "There was a server side error", error: err });
+                } else {
+                    console.log(sale);
+                    res.status(200).json({
+                        message: "Sale is created Successfully",
+                        data: sale,
+                        status: 200,
+                    });
+                }
+            });
+        } catch (err) {
+            res
+                .status(500)
+                .json({ message: "There was a server side error", error: err });
+        }
+    })
+);
+// // GET ALL CATEGORY BY GROUP
+// ecomRouter.get(
+//     "/category/group/:group",
+//     expressAsyncHandler(async (req, res) => {
+//         const group = req.params.group.toLowerCase();
+//         const categories = await Category.find({ group: group });
+//         res.send(categories);
+//     })
+// );
+// // GET ALL CATEGORY BY GROUP
+ecomRouter.get(
+    "/product/:catID",
+    expressAsyncHandler(async (req, res) => {
+        const catID = req.params.catID;
+        const product = await Product.find({ category: catID })
+            .select({
+                _id: 1,
+                name: 1,
+                ean: 1,
+                unit: 1,
+                article_code: 1,
+                priceList: 1,
+                category: 1,
+                promo_price: 1,
+                promo_start: 1,
+                promo_end: 1,
+                promo_type: 1
+            })
+            .limit(100)
+            .populate("category", "name")
+            .populate("priceList");
+        res.status(200).json(product);
+
+    })
+);
+/*==========================
+* log in register
+===========================*/
+
+
+
+module.exports = ecomRouter;
