7e6f2f284781a3302b111866ae2f16f283a12825
PROTTASAKARIM <prottasakarim99@gmail.com>
2023-02-07 17:34:10 +0600

    coustomer model update and pos screen coustomer modal edit

--- models/customerModel.js
+++ models/customerModel.js
@@ -3,11 +3,33 @@ const mongoose = require("mongoose");
 const customerSchema = mongoose.Schema(
   {
     name: { type: String, require: true },
-    email: { type: String, unique: true, sparse: true, default: null },
+    email: { type: String, unique: true, sparse: true },
     username: { type: String },
     password: { type: String },
-    // membership: { type: String, unique: true },
-    address: { type: String },
+    membership: { type: String, unique: true },
+    address: [
+      {
+        type: Map,
+        of: new mongoose.Schema({
+          type: { type: String },
+          holdingNo: { type: String },
+          sector: { type: String },
+          street: { type: String },
+          town: { type: String },
+          city: { type: String },
+          division: { type: String },
+          country: { type: String },
+          zipCode: { type: String },
+
+        },
+          {
+            timestamps: true,
+          }
+        ),
+      },
+
+    ],
+    // address: { type: String },
     type: {
       type: String,
       enum: ["regular", "premium", "vip"],
