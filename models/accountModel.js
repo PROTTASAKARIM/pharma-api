3908ee26cf658a56d8df223561f7856de956b6ea
Prottasa Karim <prottasakarim99@gmail.com>
2023-04-10 15:52:26 +0600

    working on accounts design

--- models/accountModel.js
+++ models/accountModel.js
@@ -5,23 +5,28 @@ const accountSchema = mongoose.Schema(
         acId: { type: String, require: true },
         supplier: { type: mongoose.Types.ObjectId, ref: "Supplier" },
         paidAmount: new mongoose.Schema({
-            cash: { type: Number },
+            cash: {
+                phone: { type: String },
+                amount: { type: Number },
+            },
             mfs: {
-                name: { type: String },
+                type: { type: String },
                 phone: { type: String },
                 amount: { type: Number },
             },
             card: {
-                name: { type: String },
+                type: { type: String },
                 cardNo: { type: String },
                 amount: { type: Number },
             },
             cheque: {
-                name: { type: String },
+
+                bank: { type: String },
                 chequeNo: { type: String },
                 amount: { type: Number },
             },
         }),
+        name: { type: String },
         photo: { type: String },
         accountHead: { type: mongoose.Types.ObjectId, ref: "AccountHead" },
         user: { type: mongoose.Types.ObjectId, ref: "User" },
@@ -29,6 +34,7 @@ const accountSchema = mongoose.Schema(
         amount: { type: String },
         due: { type: String },
         type: { type: String, enum: ["Debit", "Credit"] },
+        paymentType: { type: String, enum: ["Advance", "Payment"] },
         grnId: { type: mongoose.Types.ObjectId, ref: "Grn" },
         poId: { type: mongoose.Types.ObjectId, ref: "Purchase" },
         note: { type: String },
