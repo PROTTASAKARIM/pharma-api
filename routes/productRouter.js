247b1bbf960e765ac95d3af58008db41dbcb4dca
Prottasa Karim <prottasakarim99@gmail.com>
2023-05-07 14:46:33 +0600

    price router edit

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -31,61 +31,8 @@ router.get(
   })
 );
 
-router.get(
-  "/export",
-  expressAsyncHandler(async (req, res) => {
-    const products = await Product.find().select({
-      _id: 1,
-      name: 1,
-      article_code: 1,
-      unit: 1,
-      category: 1,
 
-    })
-      .populate("category", { name: 1, code: 1, mcId: 1, group: 1 });
-    res.status(200).json(products);
-  })
-);
-//get all the best selling products
-router.get(
-  "/best-seller",
-  expressAsyncHandler(async (req, res) => {
 
-    try {
-      const product = await Sale.aggregate([
-        {
-          $unwind: '$products'
-        },
-        {
-          $group: {
-            _id: '$products.id',
-            article_code: { $first: '$products.article_code' },
-            totalQuantity: { $sum: '$products.qty' },
-            name: { $first: '$products.name' },
-            mrp: { $last: '$products.mrp' },
-
-          }
-        },
-        {
-          $sort: { totalQuantity: -1 }
-        }
-      ])
-      // let pProduct_AC = []
-      // product.map(pro => {
-      //   pProduct_AC = [...pProduct_AC, pro.article_code]
-      // })
-      // console.log("pProduct", pProduct_AC)
-
-      // const productDetails = await Product.find({ article_code: pProduct_AC }).populate("priceList")
-      // console.log("pProduct new", productDetails)
-      // const fProducts = productDetails.filter(p => p.priceList.length > 0)
-      // .populate("priceList");
-      res.send(product);
-    } catch (err) {
-      console.log(err);
-    }
-  })
-);
 
 // GET PRODUCT PRICE
 router.get(
@@ -153,15 +100,10 @@ router.get(
           unit: 1,
           article_code: 1,
           priceList: 1,
-          category: 1,
           photo: 1,
-          promo_price: 1,
-          promo_start: 1,
-          promo_end: 1,
-          promo_type: 1,
+
         })
         .limit(100)
-        .populate("category", "name")
         .populate("priceList");
       res.status(200).json(product);
     } else {
@@ -178,16 +120,11 @@ router.get(
           unit: 1,
           article_code: 1,
           priceList: 1,
-          category: 1,
           photo: 1,
-          promo_price: 1,
-          promo_start: 1,
-          promo_end: 1,
-          promo_type: 1,
+
         })
         .limit(size)
         .skip(size * page)
-        .populate("category", "name")
         .populate("priceList");
       res.status(200).json(product);
       console.log("done:", query);
@@ -206,12 +143,8 @@ router.get(
           ean: 1,
           article_code: 1,
           priceList: 1,
-          category: 1,
-          promo_price: 1,
-          promo_start: 1,
-          promo_end: 1,
+
         })
-        .populate("category", "name")
         .populate("priceList");
 
       res.send(products);
@@ -221,123 +154,7 @@ router.get(
   })
 );
 
-// GET ALL ACTIVE PRODUCTS UPDATED
-router.get(
-  "/active",
-  expressAsyncHandler(async (req, res) => {
-    try {
-      const products = await Product.findActive();
-      res.send(products);
-    } catch {
-      res.status(500).json("Server side error");
-    }
-  })
-);
 
-// GET ALL PRODUCTS BY CATEGORY
-router.get(
-  "/pro",
-  expressAsyncHandler(async (req, res) => {
-    const products = await Product.find({
-      priceList: { $exists: true, $not: { $size: 0 } },
-    }).select({
-      article_code: 1,
-      name: 1,
-      priceList: 1,
-    });
-    res.send(products);
-  })
-);
-// GET ALL PRODUCTS BY CATEGORY
-// router.get(
-//   "/category/:category",
-//   expressAsyncHandler(async (req, res) => {
-//     const category = req.params.category;
-//     const products = await Product.find({
-//       category: { $regex: new RegExp("^" + category.toLowerCase(), "i") },
-//     });
-//     res.send(products);
-//   })
-// );
-
-// GET ALL FEATURED PRODUCTS
-router.get(
-  "/featured",
-  expressAsyncHandler(async (req, res) => {
-    const products = await Product.find({
-      featured: true,
-    })
-      .select({
-        name: 1,
-        ean: 1,
-        article_code: 1,
-        priceList: 1,
-        category: 1,
-        promo_price: 1,
-        promo_start: 1,
-        promo_end: 1,
-        promo_type: 1,
-      })
-      .populate("category", "name")
-      .populate("priceList");
-    res.send(products);
-  })
-);
-
-// GET ONE PRODUCT FOR PRICE TABLE
-router.get(
-  "/infoPrice/:id",
-  expressAsyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const products = await Product.find({ _id: id })
-      .select({
-        _id: 1,
-        name: 1,
-        ean: 1,
-        unit: 1,
-        article_code: 1,
-        priceList: 1,
-        category: 1,
-        master_category: 1,
-        promo_price: 1,
-        promo_start: 1,
-        promo_end: 1,
-        promo_type: 1,
-      })
-      .populate("category", "name")
-      .populate("priceList", "mrp")
-      .populate("master_category", "name");
-    res.status(200).json(products[0]);
-  })
-);
-
-// GET ONE PRODUCT inventory
-router.get(
-  "/inventory/:id",
-  expressAsyncHandler(async (req, res) => {
-    const id = req.params.id;
-    try {
-      const product = await Product.aggregate([
-        {
-          $lookup: {
-            from: "inventory",
-            localField: "article_code",
-            foreignField: "article_code",
-            as: "common",
-          },
-        },
-        {
-          $match: {
-            article_code: id,
-          },
-        },
-      ]);
-      res.send(product);
-    } catch (err) {
-      console.log(err);
-    }
-  })
-);
 // GET ONE PRODUCT
 router.get(
   "/:id",
@@ -351,89 +168,10 @@ router.get(
     res.send(products[0]);
   })
 );
-// GET ONE PRODUCT
-router.get(
-  "/promo-update/:id",
-  expressAsyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const products = await Product.find({ _id: id })
-      .populate("category", "name")
-      .populate("priceList", { mrp: 1, tp: 1, supplier: 1, _id: 1, status: 1 })
-      .populate("promo_price", "mrp");
-    res.send(products[0]);
-  })
-);
-router.get(
-  "/select/:id",
-  expressAsyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const products = await Product.find({ _id: id })
-      .select({
-        _id: 1,
-        name: 1,
-        ean: 1,
-        vat: 1,
-        unit: 1,
-        article_code: 1,
-        priceList: 1,
-        category: 1,
-        master_category: 1,
-      })
-      .populate("category", "name")
-      .populate("master_category", "name")
-      .populate("priceList", { mrp: 1, tp: 1, supplier: 1, _id: 1, status: 1 });
-    // .populate("priceList");
-    res.send(products[0]);
-  })
-);
 
-// GET PRODUCT DETAILS FOR PURCHASE PRODUCT IMPORT
-router.get(
-  "/pro-details/:id",
-  expressAsyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const products = await Product.find({ article_code: id })
-      .select({
-        _id: 1,
-        name: 1,
-        unit: 1,
-        article_code: 1,
-        priceList: 1,
-        ean: 1,
-        promo_start: 1,
-        promo_end: 1,
-        promo_price: 1,
-        promo_type: 1,
-      })
-      .populate("priceList", { mrp: 1, tp: 1, _id: 1 });
-    // .populate("priceList");
-    res.send(products[0]);
-  })
-);
 
-router.get(
-  "/details/:id",
-  expressAsyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const products = await Product.find({ _id: id })
-      .select({
-        _id: 1,
-        name: 1,
-        ean: 1,
-        vat: 1,
-        unit: 1,
-        article_code: 1,
-        priceList: 1,
-        ean: 1,
-        promo_start: 1,
-        promo_end: 1,
-        promo_price: 1,
-        promo_type: 1,
-      })
-      .populate("priceList", { mrp: 1, tp: 1, supplier: 1, _id: 1, status: 1 });
-    res.send(products[0]);
-  })
-);
+
+
 
 // GET ONE PRODUCT BY ARTICLE CODE
 router.get(
@@ -459,152 +197,7 @@ router.get(
   })
 );
 
-// GET MANY PRODUCT BY ARTICLE CODE
-router.post(
-  "/acode/:id",
-  expressAsyncHandler(async (req, res) => {
-    const id = req.data;
-    const products = await Product.find({ article_code: id })
-      .select({
-        _id: 1,
-        name: 1,
-        ean: 1,
-        vat: 1,
-        unit: 1,
-        article_code: 1,
-        priceList: 1,
-        promo_start: 1,
-        promo_end: 1,
-        promo_price: 1,
-        promo_type: 1,
-      })
-      .populate("priceList", { mrp: 1, tp: 1, _id: 0 });
-    res.send(products[0]);
-  })
-);
 
-// GET PRODUCT BY category
-router.get(
-  "/category/:id",
-  expressAsyncHandler(async (req, res) => {
-    const category = req.params.id;
-    const products = await Product.find({ category: category })
-      .select({
-        _id: 1,
-        name: 1,
-        ean: 1,
-        unit: 1,
-        vat: 1,
-        article_code: 1,
-        priceList: 1,
-        promo_price: 1,
-        promo_start: 1,
-        promo_end: 1,
-        promo_type: 1,
-      })
-      .populate("priceList", "mrp");
-    // .limit(5);
-    // Select fields
-    // console.log(products);
-    // console.log(category);
-    res.send(products);
-  })
-);
-
-// GET ONE PRODUCT BY EAN
-router.get(
-  "/ean/:id",
-  expressAsyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const products = await Product.find({ ean: id });
-    res.send(products);
-  })
-);
-
-// E COMMERCE SEARCH
-router.get(
-  "/web-search/:q",
-  expressAsyncHandler(async (req, res) => {
-    let payload = req.params.q.trim().toString().toLocaleLowerCase();
-
-    query = { name: { $regex: new RegExp("^" + payload + ".*", "i") } };
-    const search = await Product.find(query)
-      .select({
-        _id: 1,
-        name: 1,
-        ean: 1,
-        unit: 1,
-        article_code: 1,
-        photo: 1,
-        priceList: 1,
-        category: 1,
-      })
-      .limit(10);
-    if (payload === "") {
-      res.send([]);
-    } else {
-      res.send({ search });
-    }
-  })
-);
-
-// PRODUCTS SRARCH
-router.get(
-  "/search/:q",
-  expressAsyncHandler(async (req, res) => {
-    // let payload = req.query?.q?.trim().toString().toLocaleLowerCase();
-    const payload = req.params?.q?.trim().toString().toLocaleLowerCase();
-    // console.log(payload);
-
-    const isNumber = /^\d/.test(payload);
-    let query = {};
-    if (!isNumber) {
-      // query = { name: { $regex: new RegExp("^" + payload + ".*", "i") } };
-      query = { name: { $regex: new RegExp("\\b" + payload + ".*?", "i") } };
-      // name: { $regex: new RegExp(`/\b${payload}[^\b]*?\b/gi`, "i") },
-      // query = {
-      //   name: { $regex: new RegExp("\\b" + payload + "\\w+  ", "i") },
-      //   // name: { $regex: new RegExp(payload + `/\b${payload}[^\b]*?w+/g`, "i") },
-      // };
-
-      // query = { name:  payload  };
-    } else {
-      query = {
-        $or: [
-          { ean: { $regex: new RegExp("^" + payload + ".*", "i") } },
-          { article_code: { $regex: new RegExp("^" + payload + ".*", "i") } },
-        ],
-      };
-    }
-    // console.log(query);
-
-    // console.log(payload);
-
-    const search = await Product.find(query)
-      .select({
-        _id: 1,
-        name: 1,
-        ean: 1,
-        unit: 1,
-        vat: 1,
-        article_code: 1,
-        photo: 1,
-        priceList: 1,
-        promo_start: 1,
-        promo_end: 1,
-        promo_price: 1,
-        promo_type: 1,
-      })
-      .populate("priceList", { mrp: 1, tp: 1, supplier: 1, status: 1 })
-      .limit(10);
-    if (payload === "") {
-      res.send([]);
-    } else {
-      // console.log(search);
-      res.send(search);
-    }
-  })
-);
 
 // CREATE ONE PRODUCT
 router.post(
@@ -653,14 +246,9 @@ router.put(
   expressAsyncHandler(async (req, res) => {
     const id = req.params.id;
     const update = req.body;
-    console.log("update product", update);
-    const start = startOfDay(new Date(update.promo_start));
-    const end = endOfDay(new Date(update.promo_end));
-    console.log("start", start, "end", end);
-    const newProduct = { ...update, promo_start: start, promo_end: end };
-    console.log("newProduct", newProduct);
+
     try {
-      await Product.updateOne({ _id: id }, { $set: newProduct })
+      await Product.updateOne({ _id: id }, { $set: update })
         .then((response) => {
           console.log("update", response);
           res.send(response);
@@ -696,27 +284,7 @@ router.put(
     }
   })
 );
-router.put(
-  "/price/:id",
-  expressAsyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const update = { priceList: req.body.newP };
-    console.log(id, update)
-    try {
-      await Product.updateOne({ _id: id }, { $set: update })
-        .then((response) => {
-          console.log("update", response);
-          res.send(response);
-        })
-        .catch((err) => {
-          console.log("update", response);
-          res.send(err);
-        });
-    } catch (error) {
-      console.error(error);
-    }
-  })
-);
+
 // DELETE PRODUCT
 router.delete(
   "/:id",
@@ -737,24 +305,7 @@ router.delete(
     }
   })
 );
-// DELETE PRODUCT
-router.delete(
-  "/test/:id",
-  expressAsyncHandler(async (req, res) => {
-    const id = req.params.id;
-    try {
-      await Product.deleteOne({ _id: id })
-        .then((response) => {
-          res.send(response);
-        })
-        .catch((err) => {
-          res.send(err);
-        });
-    } catch (error) {
-      console.error(error);
-    }
-  })
-);
+
 
 // PRODUCT PHOTO UPLOAD
 // Upload Endpoint
@@ -809,37 +360,6 @@ router.post(
   })
 );
 
-// UPDATE PRODUCT PHOTO FIELD
-// router.post("/emptyPhoto", async (req, res) => {
-//   try {
-//     const hi = await Product.updateMany(
-//       { product_type: "standard" },
-//       { $set: { photo: "" } }
-//     );
-//     res.send(hi);
-//     // await Product.find({ product_type: "standard" }).update({
-//     //   $set: { photo: "" },
-//     console.log(hi);
-//     // });
-//   } catch (err) {
-//     console.log("err");
-//   }
-// });
 
-// // DELETE ALL
-// router.get(
-//   "/del-all",
-//   expressAsyncHandler(async (req, res) => {
-//     try {
-//       await Product.remove((response) => {
-//         res.send(response);
-//       }).catch((err) => {
-//         res.send(err);
-//       });
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   })
-// );
 
 module.exports = router;
